bde06ff130568fd80bd712bf4f4aef900937e2da
adiseys16 <adiseptiadi.mobile@gmail.com>
2026-09-16 16:31:05 +0700

    hot fix unused

--- android/src/main/kotlin/com/screenshot/recording/detector/screenshot_recording_detector/ScreenshotRecordingDetectorPlugin.kt
+++ android/src/main/kotlin/com/screenshot/recording/detector/screenshot_recording_detector/ScreenshotRecordingDetectorPlugin.kt
@@ -12,7 +12,6 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
 import android.provider.MediaStore
-import android.util.Log
 import android.view.WindowManager
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.Lifecycle
